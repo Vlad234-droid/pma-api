872d94c07a088f5cff99036a6c9c5285254f4eca
Yura <yurij.pashynsky@gmail.com>
2021-09-14 21:23:33 +0300

    PMA-276 | fix incorrect location of mapper and tests

--- codebase/objective/src/main/java/com/tesco/pma/objective/configuration/ObjectiveConfiguration.java
+++ codebase/objective/src/main/java/com/tesco/pma/objective/configuration/ObjectiveConfiguration.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "com.tesco.pma.colleague.objective")
+@ComponentScan(basePackages = "com.tesco.pma.objective")
 public class ObjectiveConfiguration {
 }
