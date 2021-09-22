71e2d6d083686020cbd7681eb75e50ee8e6c71f7
Andrii Kuzmin <andrii.kuzmin@tesco.com>
2021-09-22 16:07:05 +0300

    PMA-397 PMA|Backend Colleague API local data merge. More code review comments were implemented.

--- codebase/cep/src/main/java/com/tesco/pma/colleague/cep/domain/DeliveryMode.java
+++ codebase/cep/src/main/java/com/tesco/pma/colleague/cep/domain/DeliveryMode.java
@@ -1,19 +1,32 @@
 package com.tesco.pma.colleague.cep.domain;
 
+import lombok.Getter;
+
 import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /*
   @see <a href="https://github.dev.global.tesco.org/24Colleague/ColleagueEventingPlatform/wiki/Event-Producer-Colleague-Changes">here</a>
  */
+@Getter
 public enum DeliveryMode {
-    JIT,
-    IMMEDIATE;
 
-    public static DeliveryMode getDeliveryMode(String feedId) {
-        return Arrays.stream(DeliveryMode.values())
-                .filter(deliveryMode -> feedId.toLowerCase().contains(deliveryMode.name().toLowerCase()))
-                .findFirst()
-                .orElse(JIT);
+    JIT("colleagues-jit-v1"),
+    IMMEDIATE("colleagues-immediate-v1");
+
+    private final String value;
+
+    private static final Map<String, DeliveryMode> LOOKUP = Arrays.stream(DeliveryMode.values())
+            .collect(Collectors.toMap(DeliveryMode::getValue, Function.identity()));
+
+    DeliveryMode(String value) {
+        this.value = value;
+    }
+
+    public static DeliveryMode getDeliveryMode(String value) {
+        return LOOKUP.get(value);
     }
 
 }
