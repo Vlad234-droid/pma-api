9260adc44c169b4645b449ae114a46cd21b95b77
Vadim Shatokhin <vadim.shatokhin1@tesco.com>
2021-10-21 13:02:26 +0300

    Model updated

--- codebase/pm/src/main/java/com/tesco/pma/process/api/model/PMReview.java
+++ codebase/pm/src/main/java/com/tesco/pma/process/api/model/PMReview.java
@@ -15,9 +15,15 @@ import lombok.NoArgsConstructor;
 public class PMReview extends PMElement {
     public static final String PM_REVIEW = "review";
     public static final String PM_REVIEW_PREFIX = PM_PREFIX + PM_REVIEW + "_";
-    public static final String PM_REVIEW_TYPE = PM_REVIEW_PREFIX + "review_type";
-    public static final String PM_REVIEW_MIN = PM_REVIEW_PREFIX + "review_min";
-    public static final String PM_REVIEW_MAX = PM_REVIEW_PREFIX + "review_max";
+    public static final String PM_REVIEW_TYPE = PM_REVIEW_PREFIX + "type";
+    public static final String PM_REVIEW_MIN = PM_REVIEW_PREFIX + "min";
+    public static final String PM_REVIEW_MAX = PM_REVIEW_PREFIX + "max";
+    public static final String PM_REVIEW_START = PM_REVIEW_PREFIX + "start_time";
+    public static final String PM_REVIEW_END = PM_REVIEW_PREFIX + "end_time";
+    public static final String PM_REVIEW_DURATION = PM_REVIEW_PREFIX + "duration";
+    public static final String PM_REVIEW_NOTIFY_DELAY = PM_REVIEW_PREFIX + "notify_delay";
+    public static final String PM_REVIEW_PRE_NOTIFY_BEFORE = PM_REVIEW_PREFIX + "pre_notify_before";
+    public static final String PM_REVIEW_PRE_NOTIFY_START_TIME = PM_REVIEW_PREFIX + "pre_notify_start_time";
 
     public static final String DEFAULT_PM_REVIEW_MIN = "1";
     public static final String DEFAULT_PM_REVIEW_MAX = "1";
