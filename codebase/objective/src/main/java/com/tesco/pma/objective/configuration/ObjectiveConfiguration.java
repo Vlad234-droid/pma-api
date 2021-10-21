56a2707d836ef125df591d6b2570a4997490d71c
Vadim Shatokhin <vadim.shatokhin1@tesco.com>
2021-10-21 16:59:40 +0300

    Objective to review

--- codebase/objective/src/main/java/com/tesco/pma/objective/configuration/ObjectiveConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.tesco.pma.objective.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan(basePackages = "com.tesco.pma.objective")
-public class ObjectiveConfiguration {
-}
---
+++ codebase/review/src/test/java/com/tesco/pma/review/LocalTestConfig.java
@@ -0,0 +1,15 @@
+package com.tesco.pma.review;
+
+import com.tesco.pma.configuration.MessageSourceConfig;
+import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+@Import({MessageSourceAutoConfiguration.class,
+        MessageSourceConfig.class})
+public class LocalTestConfig {
+
+}
