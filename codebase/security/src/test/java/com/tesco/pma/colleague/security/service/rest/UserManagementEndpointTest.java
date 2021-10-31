2292eba0191961ef8fe2f3b2831f9599a1a8bbbd
Andrii Kuzmin <andrii.kuzmin@tesco.com>
2021-10-31 17:55:11 +0200

    PMA-576 Backend PMA app API security integration. Unit tests for controller layer were implemented.

--- codebase/security/src/test/java/com/tesco/pma/colleague/security/service/rest/UserManagementEndpointTest.java
+++ codebase/security/src/test/java/com/tesco/pma/colleague/security/service/rest/UserManagementEndpointTest.java
@@ -1,17 +1,41 @@
 package com.tesco.pma.colleague.security.service.rest;
 
+import com.tesco.pma.colleague.security.domain.Account;
+import com.tesco.pma.colleague.security.domain.Role;
+import com.tesco.pma.colleague.security.service.UserManagementService;
 import com.tesco.pma.rest.AbstractEndpointTest;
+import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-@WebMvcTest(controllers = UserManagementEndpoint.class)
+import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = UserManagementEndpoint.class, properties = {
+        "tesco.application.security.enabled=false",
+})
 // TODO Implement all tests
 class UserManagementEndpointTest extends AbstractEndpointTest {
 
+    private static final EasyRandom RANDOM = new EasyRandom();
+
+    @Autowired
+    protected MockMvc mvc;
+
+    @MockBean
+    private UserManagementService mockUserManagementService;
 
     @BeforeEach
     void setUp() {
@@ -22,7 +46,16 @@ class UserManagementEndpointTest extends AbstractEndpointTest {
     }
 
     @Test
-    void getAccounts() {
+    void getAccountsShouldReturnAllAccounts() throws Exception {
+
+        when(mockUserManagementService.getAccounts())
+                .thenReturn(randomObjects(Account.class, 3));
+
+        mvc.perform(get("/user-management/accounts")
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON));
+
     }
 
     @Test
@@ -34,7 +67,16 @@ class UserManagementEndpointTest extends AbstractEndpointTest {
     }
 
     @Test
-    void getRoles() {
+    void getRolesShouldReturnAllRoles() throws Exception {
+
+        when(mockUserManagementService.getRoles())
+                .thenReturn(randomObjects(Role.class, 3));
+
+        mvc.perform(get("/user-management/roles")
+                        .accept(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(APPLICATION_JSON));
+
     }
 
     @Test
@@ -43,6 +85,12 @@ class UserManagementEndpointTest extends AbstractEndpointTest {
 
     @Test
     void revokeRole() {
+    }
+
+    private <T> List<T> randomObjects(Class<T> type, int size) {
+        return IntStream.rangeClosed(1, size)
+                .mapToObj(value -> RANDOM.nextObject(type))
+                .collect(Collectors.toList());
     }
 
 }
