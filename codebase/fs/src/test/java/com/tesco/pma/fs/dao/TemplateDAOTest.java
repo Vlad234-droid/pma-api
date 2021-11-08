48a385ec30d23f953a5edaf21ea9b0ea1c2160f1
Victoria Silenko <Victoria.Silenko@tesco.com>
2021-11-08 20:26:05 +0200

    PMA-545 dao tests

---
+++ codebase/fs/src/test/java/com/tesco/pma/fs/dao/TemplateDAOTest.java
@@ -0,0 +1,86 @@
+package com.tesco.pma.fs.dao;
+
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
+import com.tesco.pma.dao.AbstractDAOTest;
+import com.tesco.pma.fs.domain.ProcessTemplate;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+
+import java.time.Instant;
+import java.util.UUID;
+
+import static com.tesco.pma.fs.domain.ProcessTemplateStatus.ACTIVE;
+import static com.tesco.pma.fs.domain.ProcessTemplateType.FORM;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TemplateDAOTest extends AbstractDAOTest {
+
+    private static final String BASE_PATH_TO_DATA_SET = "com/tesco/pma/fs/dao/";
+
+    private static final UUID TEMPLATE_UUID_1 = UUID.fromString("6d37262f-3a00-4706-a74b-6bf98be65765");
+    private static final UUID TEMPLATE_UUID_2 = UUID.fromString("3d1ebbee-bafc-467c-acf1-5334db06e723");
+    private static final String PATH = "/home/vsilenko/dev";
+
+    @Autowired
+    private TemplateDAO instance;
+
+    @DynamicPropertySource
+    static void postgresqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.default.jdbc-url", CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.default.password", CONTAINER::getPassword);
+        registry.add("spring.datasource.default.username", CONTAINER::getUsername);
+    }
+
+    @Test
+    @DataSet(BASE_PATH_TO_DATA_SET + "process_template_init.xml")
+    void findTemplateByUuid() {
+        final var result = instance.findTemplateByUuid(TEMPLATE_UUID_1, true);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getUuid()).isEqualTo(TEMPLATE_UUID_1);
+    }
+
+    @Test
+    @DataSet(BASE_PATH_TO_DATA_SET + "process_template_init.xml")
+    void findAllTemplates() {
+        final var result = instance.findAllTemplates(false);
+
+        assertThat(result).isNotEmpty();
+        assertThat(result.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DataSet(BASE_PATH_TO_DATA_SET + "process_template_init.xml")
+    void getMaxVersionForTemplate() {
+        final var result = instance.getMaxVersionForTemplate(PATH, "test1.txt");
+
+        assertThat(result).isEqualTo(2);
+    }
+
+    @Test
+    @DataSet(BASE_PATH_TO_DATA_SET + "cleanup.xml")
+    @ExpectedDataSet(BASE_PATH_TO_DATA_SET + "process_template_create_expected.xml")
+    void createTemplateSucceeded() {
+        final var template = new ProcessTemplate();
+        template.setUuid(TEMPLATE_UUID_2);
+        template.setPath(PATH);
+        template.setVersion(1);
+        template.setType(FORM);
+        template.setStatus(ACTIVE);
+        template.setDescription("other template");
+        template.setCreatedBy("test");
+        template.setCreatedTime(Instant.parse("2021-11-03T22:38:14Z"));
+        template.setFileName("test3.txt");
+        template.setFileDate(Instant.parse("2021-04-22T08:50:08Z"));
+        template.setFileLength(0);
+        template.setFileContent(new byte[] {});
+
+        final int rowsInserted = instance.saveTemplate(template);
+
+        assertThat(rowsInserted).isOne();
+    }
+
+}
