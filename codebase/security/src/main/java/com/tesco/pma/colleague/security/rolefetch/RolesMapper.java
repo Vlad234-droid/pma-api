9a0713886db7a22216c84069478948c090562941
Andrii Kuzmin <andrii.kuzmin@tesco.com>
2021-11-18 12:34:10 +0200

    PMA-775 PMA|Backend Amend role fetching. Added Javadoc.

--- codebase/security/src/main/java/com/tesco/pma/colleague/security/rolefetch/RolesMapper.java
+++ codebase/security/src/main/java/com/tesco/pma/colleague/security/rolefetch/RolesMapper.java
@@ -7,12 +7,20 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Processing configuration role mapping
+ */
 @Data
 @NoArgsConstructor
 public class RolesMapper {
 
     private Map<String, String> rolesMapping = new HashMap<>();
 
+    /**
+     *
+     * @param code - Tesco group
+     * @return PMA Role
+     */
     public String findRoleByCode(String code) {
         return Optional.ofNullable(rolesMapping.get(code)).orElse("");
     }
