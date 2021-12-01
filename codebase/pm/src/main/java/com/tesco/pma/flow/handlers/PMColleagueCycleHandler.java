d00d0fadc1d4c862023579848425d7fa17c05c74
mdiadia <mykyta.diadia@tesco.com>
2021-12-01 14:58:29 +0200

    PMA-797 | PM Timeline points

--- codebase/pm/src/main/java/com/tesco/pma/flow/handlers/PMColleagueCycleHandler.java
+++ codebase/pm/src/main/java/com/tesco/pma/flow/handlers/PMColleagueCycleHandler.java
@@ -1,23 +1,20 @@
 package com.tesco.pma.flow.handlers;
 
 import com.tesco.pma.bpm.api.flow.ExecutionContext;
-import com.tesco.pma.bpm.camunda.flow.CamundaExecutionContext;
 import com.tesco.pma.bpm.camunda.flow.handlers.CamundaAbstractFlowHandler;
 import com.tesco.pma.cycle.api.PMColleagueCycle;
 import com.tesco.pma.cycle.api.PMCycle;
 import com.tesco.pma.cycle.service.PMColleagueCycleService;
 import com.tesco.pma.organisation.service.ConfigEntryService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 import java.util.stream.Collectors;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
-public class PmColleagueCycleHandler extends CamundaAbstractFlowHandler {
+public class PMColleagueCycleHandler extends CamundaAbstractFlowHandler {
 
     private final ConfigEntryService configEntryService;
     private final PMColleagueCycleService pmColleagueCycleService;
@@ -31,10 +28,6 @@ public class PmColleagueCycleHandler extends CamundaAbstractFlowHandler {
                 .collect(Collectors.toList());
 
         pmColleagueCycleService.saveColleagueCycles(colleagueCycles);
-
-
-        log.info("Context variables: \n" + (((CamundaExecutionContext) context).getDelegateExecution()).getVariables());
-        log.info("Local variables: \n" + (((CamundaExecutionContext) context).getDelegateExecution()).getVariablesLocal());
     }
 
     private PMColleagueCycle mapToColleagueCycle(UUID colleagueUuid, PMCycle cycle) {
