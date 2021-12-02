04bdb58df620e21c0edd2459b124c3eb0ab87b23
Vadim Shatokhin <UKE12375175@tesco.com>
2021-12-02 10:49:54 +0200

    fixes

--- codebase/pm/src/test/java/com/tesco/pma/process/dao/PMRuntimeProcessDAOTest.java
+++ codebase/pm/src/test/java/com/tesco/pma/process/dao/PMRuntimeProcessDAOTest.java
@@ -1,20 +1,19 @@
 package com.tesco.pma.process.dao;
 
-import java.util.List;
-import java.util.UUID;
-
+import com.github.database.rider.core.api.dataset.DataSet;
+import com.tesco.pma.api.DictionaryFilter;
 import com.tesco.pma.cycle.dao.config.PMCycleTypeHandlerConfig;
+import com.tesco.pma.dao.AbstractDAOTest;
+import com.tesco.pma.process.api.PMProcessStatus;
+import com.tesco.pma.process.api.PMRuntimeProcess;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
-import com.github.database.rider.core.api.dataset.DataSet;
-import com.tesco.pma.api.DictionaryFilter;
-import com.tesco.pma.dao.AbstractDAOTest;
-import com.tesco.pma.process.api.PMRuntimeProcess;
-import com.tesco.pma.process.api.PMProcessStatus;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
@@ -92,10 +91,10 @@ class PMRuntimeProcessDAOTest extends AbstractDAOTest {
         assertTrue(processes.get(0).getLastUpdateTime().isAfter(processes.get(1).getLastUpdateTime()));
     }
 
-    private void checkProcess(PMRuntimeProcess actual, UUID pmUuid, PMProcessStatus registered, String bpmUuid) {
+    private void checkProcess(PMRuntimeProcess actual, UUID pmUuid, PMProcessStatus status, String bpmUuid) {
         assertNotNull(actual);
         assertEquals(pmUuid, actual.getId());
-        assertEquals(registered, actual.getStatus());
+        assertEquals(status, actual.getStatus());
         assertEquals(CYCLE_UUID, actual.getCycleUuid());
         assertEquals(bpmUuid, actual.getBpmProcessId());
         assertEquals(BUSINESS_KEY, actual.getBusinessKey());
