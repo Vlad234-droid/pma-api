7355c5446927d606f28887b7f75c34dc591ea60a
Victoria Silenko <Victoria.Silenko@tesco.com>
2021-12-22 00:07:44 +0200

    PMA-895 PDP refactoring, controller implementation, service, validation

--- codebase/pdp/src/main/java/com/tesco/pma/pdp/rest/PDPEndpoint.java
+++ codebase/pdp/src/main/java/com/tesco/pma/pdp/rest/PDPEndpoint.java
@@ -1,5 +1,6 @@
 package com.tesco.pma.pdp.rest;
 
+import com.tesco.pma.pdp.service.PDPService;
 import com.tesco.pma.rest.HttpStatusCodes;
 import com.tesco.pma.rest.RestResponse;
 import com.tesco.pma.pdp.domain.PDPGoal;
@@ -9,6 +10,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,16 +21,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.UUID;
 
+import static com.tesco.pma.rest.RestResponse.success;
+
 @RestController
 @RequestMapping(path = "/pdp")
+@Validated
 @RequiredArgsConstructor
 public class PDPEndpoint {
 
+    private final PDPService pdpService;
+
     /**
-     * POST call to create a PDP.
+     * POST call to create a PDP with its Goals.
      *
      * @param colleagueUuid an identifier of colleague
      * @param goals         a list of PDP goals
@@ -38,12 +46,29 @@ public class PDPEndpoint {
     @ApiResponse(responseCode = HttpStatusCodes.CREATED, description = "Successful operation")
     @PostMapping(path = "/{colleagueUuid}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
-    public RestResponse<List<PDPGoal>> createPDP(@PathVariable("colleagueUuid") UUID colleagueUuid, @RequestBody List<PDPGoal> goals) {
-        return null;
+    public RestResponse<List<PDPGoal>> createPDP(@PathVariable("colleagueUuid") UUID colleagueUuid,
+                                                 @RequestBody List<@Valid PDPGoal> goals) {
+        return success(pdpService.createPDP(colleagueUuid, goals));
     }
 
     /**
-     * POST call to update a PDP.
+     * POST call to add a PDP Goal to a Plan.
+     *
+     * @param colleagueUuid an identifier of colleague
+     * @param goal          a PDP goal
+     * @return a RestResponse parameterized with Goal of PDP
+     */
+    @Operation(summary = "Add a PDP Goal to a Plan", description = "PDP Goal created", tags = {"pdp"})
+    @ApiResponse(responseCode = HttpStatusCodes.CREATED, description = "Successful operation")
+    @PostMapping(path = "/{colleagueUuid}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public RestResponse<PDPGoal> addPDPGoal(@PathVariable("colleagueUuid") UUID colleagueUuid,
+                                            @RequestBody @Valid PDPGoal goal) {
+        return success(pdpService.addPDPGoal(colleagueUuid, goal));
+    }
+
+    /**
+     * POST call to update a PDP with its Goals.
      *
      * @param colleagueUuid an identifier of colleague
      * @param goals         a list of PDP goals
@@ -52,71 +77,85 @@ public class PDPEndpoint {
     @Operation(summary = "Update a PDP", description = "PDP updated", tags = {"pdp"})
     @ApiResponse(responseCode = HttpStatusCodes.OK, description = "PDP updated")
     @PutMapping(path = "/{colleagueUuid}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public RestResponse<List<PDPGoal>> updatePDP(@PathVariable("colleagueUuid") UUID colleagueUuid, @RequestBody List<PDPGoal> goals) {
-        return null;
+    public RestResponse<List<PDPGoal>> updatePDP(@PathVariable("colleagueUuid") UUID colleagueUuid,
+                                                 @RequestBody List<@Valid PDPGoal> goals) {
+        return success(pdpService.updatePDP(colleagueUuid, goals));
     }
 
     /**
-     * POST call to create a PDP Goal.
-     *
-     * @param colleagueUuid an identifier of colleague
-     * @param goal          a PDP goal
-     * @return a RestResponse parameterized with Goal of PDP
-     */
-    @Operation(summary = "Create a PDP Goal", description = "PDP Goal created", tags = {"pdp"})
-    @ApiResponse(responseCode = HttpStatusCodes.CREATED, description = "Successful operation")
-    @PostMapping(path = "/{colleagueUuid}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    @ResponseStatus(HttpStatus.CREATED)
-    public RestResponse<PDPGoal> addPDPGoal(@PathVariable("colleagueUuid") UUID colleagueUuid, @RequestBody PDPGoal goal) {
-        return null;
-    }
-
-    /**
-     * DELETE call to delete a PDP Goal.
+     * DELETE call to delete a PDP Goal by its colleague and number.
      *
      * @param colleagueUuid an identifier of colleague
      * @param number        a sequence number of PDP Goal
      * @return a Void RestResponse
      */
-    @Operation(summary = "Delete existing PDP", description = "Delete existing PDP", tags = {"pdp"})
+    @Operation(summary = "Delete existing PDP Goal from a Plan by its colleague and number",
+            description = "Delete existing PDP Goal", tags = {"pdp"})
     @ApiResponse(responseCode = HttpStatusCodes.OK, description = "PDP deleted")
     @ApiResponse(responseCode = HttpStatusCodes.NOT_FOUND, description = "PDP not found", content = @Content)
-    @DeleteMapping(path = "/colleagues/{colleagueUuid}/numbers/{number}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public RestResponse<Void> deletePDP(@PathVariable("colleagueUuid") UUID colleagueUuid,
-                                           @PathVariable("number") Integer number) {
-        return null;
+    @DeleteMapping(path = "/colleagues/{colleagueUuid}/numbers/{number}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public RestResponse<Void> deletePDPGoalByColleagueAndNumber(@PathVariable("colleagueUuid") UUID colleagueUuid,
+                                                                @PathVariable("number") Integer number) {
+        pdpService.deletePDPGoalByColleagueAndNumber(colleagueUuid, number);
+        return RestResponse.success();
     }
 
     /**
-     * Get call using a Path params and return a PDP Goal as JSON.
+     * DELETE call to delete a PDP Goal by its uuid.
+     *
+     * @param goalUuid an identifier of goal
+     * @return a Void RestResponse
+     */
+    @Operation(summary = "Delete existing PDP Goal from a Plan by its uuid", description = "Delete existing PDP Goal", tags = {"pdp"})
+    @ApiResponse(responseCode = HttpStatusCodes.OK, description = "PDP deleted")
+    @ApiResponse(responseCode = HttpStatusCodes.NOT_FOUND, description = "PDP not found", content = @Content)
+    @DeleteMapping(path = "/goals/{goalUuid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public RestResponse<Void> deletePDPGoalByUuid(@PathVariable("goalUuid") UUID goalUuid) {
+        pdpService.deletePDPGoalByUuid(goalUuid);
+        return RestResponse.success();
+    }
+
+    /**
+     * Get call using a Path params and return a PDP Goal by its colleagueUuid and number as JSON.
      *
      * @param colleagueUuid an identifier of colleague
      * @param number        a sequence number of PDP Goal
      * @return a RestResponse parameterized with PDP Goal
      */
-    @Operation(summary = "Get a PDP Goal by its colleagueUuid and number", tags = {"pdp"})
+    @Operation(summary = "Get a PDP Goal by its colleague and number", tags = {"pdp"})
     @ApiResponse(responseCode = HttpStatusCodes.OK, description = "Found the PDP Goal")
     @ApiResponse(responseCode = HttpStatusCodes.NOT_FOUND, description = "PDP Goal not found", content = @Content)
-    @GetMapping(path = "/colleagues/{colleagueUuid}/numbers/{number}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public RestResponse<PDPGoal> getPDPGoal(@PathVariable("colleagueUuid") UUID colleagueUuid,
-                                            @PathVariable("number") Integer number) {
-        return null;
+    @GetMapping(path = "/colleagues/{colleagueUuid}/numbers/{number}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public RestResponse<PDPGoal> getPDPGoalByColleagueAndNumber(@PathVariable("colleagueUuid") UUID colleagueUuid,
+                                                                @PathVariable("number") Integer number) {
+        return success(pdpService.getPDPGoalByColleagueAndNumber(colleagueUuid, number));
     }
 
     /**
-     * Get call using a Path params and return a PDP Goal as JSON.
+     * Get call using a Path params and return a PDP Goal by its uuid as JSON.
      *
-     * @param colleagueUuid an identifier of colleague
+     * @param goalUuid an identifier of goal
      * @return a RestResponse parameterized with PDP Goal
      */
-    @Operation(summary = "Get a list of PDP Goal by its colleagueUuid", tags = {"pdp"})
+    @Operation(summary = "Get a PDP Goal by its uuid", tags = {"pdp"})
+    @ApiResponse(responseCode = HttpStatusCodes.OK, description = "Found the PDP Goal")
+    @ApiResponse(responseCode = HttpStatusCodes.NOT_FOUND, description = "PDP Goal not found", content = @Content)
+    @GetMapping(path = "/goals/{goalUuid}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public RestResponse<PDPGoal> getPDPGoalByUuid(@PathVariable("goalUuid") UUID goalUuid) {
+        return success(pdpService.getPDPGoalByUuid(goalUuid));
+    }
+
+    /**
+     * Get call using a Path params and return a list of PDP Goals by its colleague as JSON.
+     *
+     * @param colleagueUuid an identifier of colleague
+     * @return a RestResponse parameterized with list of PDP Goals
+     */
+    @Operation(summary = "Get a list of PDP Goals by its colleague", tags = {"pdp"})
     @ApiResponse(responseCode = HttpStatusCodes.OK, description = "Found the PDP Goals")
     @ApiResponse(responseCode = HttpStatusCodes.NOT_FOUND, description = "PDP Goals not found", content = @Content)
-    @GetMapping(path = "/colleagues/{colleagueUuid}",
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(path = "/colleagues/{colleagueUuid}", produces = MediaType.APPLICATION_JSON_VALUE)
     public RestResponse<List<PDPGoal>> getPDPGoalsByColleague(@PathVariable("colleagueUuid") UUID colleagueUuid) {
-        return null;
+        return success(pdpService.getPDPGoalsByColleague(colleagueUuid));
     }
 }
---
+++ codebase/pdp/src/main/java/com/tesco/pma/pdp/service/PDPService.java
@@ -0,0 +1,83 @@
+package com.tesco.pma.pdp.service;
+
+import com.tesco.pma.pdp.domain.PDPGoal;
+
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Personal Development Plan service.
+ * Implementation classes must be annotated with @org.springframework.validation.annotation.Validated.
+ */
+public interface PDPService {
+
+    /**
+     * Create a PDP with its Goals
+     *
+     * @param colleagueUuid is colleague identifier
+     * @param goals         are non-empty list of PDP goals
+     * @return list of created PDP goals
+     */
+    List<PDPGoal> createPDP(@NotNull UUID colleagueUuid, @NotEmpty List<PDPGoal> goals);
+
+    /**
+     * Add a PDP Goal to a Plan
+     *
+     * @param colleagueUuid is colleague identifier
+     * @param goal          is PDP goal
+     * @return added PDP goal
+     */
+    PDPGoal addPDPGoal(@NotNull UUID colleagueUuid, @NotNull PDPGoal goal);
+
+    /**
+     * Update a PDP with its Goals
+     *
+     * @param colleagueUuid is colleague identifier
+     * @param goals         are non-empty list of PDP goals
+     * @return list of updated PDP goals
+     */
+    List<PDPGoal> updatePDP(@NotNull UUID colleagueUuid, @NotEmpty List<PDPGoal> goals);
+
+    /**
+     * Delete PDP Goal from Plan by its colleague and number
+     *
+     * @param colleagueUuid is colleague identifier
+     * @param number        is goal's number
+     */
+    void deletePDPGoalByColleagueAndNumber(@NotNull UUID colleagueUuid, @NotNull Integer number);
+
+    /**
+     * Delete PDP Goal from Plan by its uuid
+     *
+     * @param goalUuid is goal identifier
+     */
+    void deletePDPGoalByUuid(@NotNull UUID goalUuid);
+
+    /**
+     * Get a PDP Goal by its colleague and number
+     *
+     * @param colleagueUuid is colleague identifier
+     * @param number        is goal's number
+     * @return PDP Goal by its colleagueUuid and number
+     */
+    PDPGoal getPDPGoalByColleagueAndNumber(@NotNull UUID colleagueUuid, @NotNull Integer number);
+
+    /**
+     * Get a PDP Goal by its uuid
+     *
+     * @param goalUuid is goal identifier
+     * @return PDP Goal by its uuid
+     */
+    PDPGoal getPDPGoalByUuid(@NotNull UUID goalUuid);
+
+    /**
+     * Get a list of PDP Goals by its colleague ordered by its number
+     *
+     * @param colleagueUuid is colleague identifier
+     * @return list of PDP Goals by its colleagueUuid
+     */
+    List<PDPGoal> getPDPGoalsByColleague(@NotNull UUID colleagueUuid);
+}
