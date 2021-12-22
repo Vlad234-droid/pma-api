616a844f4907a0144d2be65c2e18ec12b13fe7f3
mdiadia <mykyta.diadia@tesco.com>
2021-12-22 13:12:57 +0200

    PMA-943

--- codebase/flow/src/main/java/com/tesco/pma/flow/handlers/PMNewColleagueEventHandler.java
+++ codebase/flow/src/main/java/com/tesco/pma/flow/handlers/PMNewColleagueEventHandler.java
@@ -6,20 +6,22 @@ import com.tesco.pma.cycle.api.PMCycleStatus;
 import com.tesco.pma.cycle.service.PMColleagueCycleService;
 import com.tesco.pma.cycle.service.PMCycleService;
 import com.tesco.pma.event.EventParams;
-import com.tesco.pma.exception.NotFoundException;
 import com.tesco.pma.flow.FlowParameters;
+import com.tesco.pma.logging.LogFormatter;
 import com.tesco.pma.organisation.service.ConfigEntryService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
 import static com.tesco.pma.flow.exception.ErrorCodes.PM_CYCLE_NOT_FOUND_FOR_COLLEAGUE;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -39,18 +41,18 @@ public class PMNewColleagueEventHandler extends AbstractColleagueCycleHandler {
             var eventProperties = event.getEventProperties();
             if (eventProperties.containsKey(EventParams.COLLEAGUE_UUID.name())) {
                 var colleagueUuid = UUID.fromString(eventProperties.get(EventParams.COLLEAGUE_UUID.name()).toString());
-                var cycle = pmCycleService.getAll(true)
+                pmCycleService.getAll(true)
                         .stream()
                         .filter(pmCycle -> PMCycleStatus.ACTIVE == pmCycle.getStatus())
                         .filter(c -> configEntryService.isColleagueExistsForCompositeKey(colleagueUuid, c.getEntryConfigKey()))
-                        .findFirst()//todo remove exception
-                        .orElseThrow(() -> new NotFoundException(PM_CYCLE_NOT_FOUND_FOR_COLLEAGUE.getCode(),
-                                messageSourceAccessor.getMessage(PM_CYCLE_NOT_FOUND_FOR_COLLEAGUE,
+                        .findFirst()
+                        .ifPresentOrElse(cycle -> {
+                                    pmColleagueCycleService.saveColleagueCycles(List.of(mapToColleagueCycle(colleagueUuid, cycle)));
+                                    context.setVariable(FlowParameters.PM_CYCLE, cycle);
+                                    context.setVariable(FlowParameters.START_DATE, defineStartDate(cycle));
+                                },
+                                () -> log.warn(LogFormatter.formatMessage(messageSourceAccessor, PM_CYCLE_NOT_FOUND_FOR_COLLEAGUE,
                                         Map.of(COLLEAGUE_UUID_PARAMETER, colleagueUuid))));
-
-                pmColleagueCycleService.saveColleagueCycles(Collections.singletonList(mapToColleagueCycle(colleagueUuid, cycle)));
-                context.setVariable(FlowParameters.PM_CYCLE, cycle);
-                context.setVariable(FlowParameters.START_DATE, defineStartDate(cycle));
             }
         }
     }
