45b8220e48f5ad55cacab77741214b59b987e264
Virendra Hingle <virendra.virchanderhingle@tesco.com>
2021-12-23 14:28:19 +0000

    Created the setup files to deploy api application in pte

---
+++ deployment/deploy-pte.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+kubectl config use-context euw-pte-214-pma-aks-admin
+helm secrets upgrade \
+  --install pma-api-service ./pma-api/ \
+  --set image.tag=$1 \
+  -f ./pma-api/values-pte.yaml \
+  -f ./pma-api/secrets-pte.yaml \
+  -f ./pma-api/pte/* \
+  -n api
