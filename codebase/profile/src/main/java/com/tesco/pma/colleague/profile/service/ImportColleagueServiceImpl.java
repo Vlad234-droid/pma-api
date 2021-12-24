9fdc35ef6f0dfc8ccfbc813295ffcd54e8bd05bd
Andrii Kuzmin <andrii.kuzmin@tesco.com>
2021-12-24 15:15:47 +0200

    PMA-938 Backend Colleague account creation during import. Refactoring of code structure.

--- codebase/profile/src/main/java/com/tesco/pma/colleague/profile/service/ImportColleagueServiceImpl.java
+++ codebase/profile/src/main/java/com/tesco/pma/colleague/profile/service/ImportColleagueServiceImpl.java
@@ -93,7 +93,7 @@ public class ImportColleagueServiceImpl implements ImportColleagueService {
         eventSender.sendEvents(events);
 
         events = importReport.getImported().stream().map(uuid -> {
-            var event = new EventSupport(EventNames.NEW_COLLEAGUE_ADDED);
+            var event = new EventSupport(EventNames.IMPORT_NEW_COLLEAGUE_2);
             event.setEventProperties(Map.of(EventParams.COLLEAGUE_UUID.name(), uuid));
             return event;
         }).collect(Collectors.toList());
