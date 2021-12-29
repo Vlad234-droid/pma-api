db9e1245cbc33d9d7e74d3db2f951add621f5f83
Andrii Kuzmin <andrii.kuzmin@tesco.com>
2021-12-29 18:24:49 +0200

    PMA-975 Backend Create default user to allow performance testing. An initial implementation.

---
+++ codebase/security/src/main/java/com/tesco/pma/configuration/security/testing/DefaultUserConfiguration.java
@@ -0,0 +1,50 @@
+package com.tesco.pma.configuration.security.testing;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Profile("performance")
+@Configuration
+public class DefaultUserConfiguration {
+
+    @Value("${tesco.application.security.performance-testing.default-user.username:10000000-0000-0000-0000-000000000000}")
+    private String userName;
+
+    private static final int DEFAULT_USER_FILTER_PRECEDENCE = 107;
+    private static final String[] DEFAULT_USER_URL_PATTERNS = {"/*"};
+
+    /**
+     * Register the servlet filter for setting default user authentication
+     *
+     * @return FilterRegistrationBean
+     */
+    @Bean
+    @ConditionalOnProperty(name = "tesco.application.security.enabled", havingValue = "false")
+    public FilterRegistrationBean<DefaultUserFilter> defaultUserFilter() {
+        FilterRegistrationBean<DefaultUserFilter> filterRegistration = new FilterRegistrationBean<>();
+        filterRegistration.setName("pmaDefaultUserFilter");
+        filterRegistration.setFilter(new DefaultUserFilter(createDefaultUserAuthentication()));
+        // Make sure the filter is registered after the Spring Security Filter Chain
+        filterRegistration.setOrder(DEFAULT_USER_FILTER_PRECEDENCE);
+        filterRegistration.addUrlPatterns(DEFAULT_USER_URL_PATTERNS);
+        return filterRegistration;
+    }
+
+    private Authentication createDefaultUserAuthentication() {
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        var user = new User(userName, "password", grantedAuthorities);
+        return new UsernamePasswordAuthenticationToken(user, user.getPassword(), user.getAuthorities());
+    }
+
+}
---
+++ codebase/security/src/main/java/com/tesco/pma/configuration/security/testing/DefaultUserFilter.java
@@ -0,0 +1,37 @@
+package com.tesco.pma.configuration.security.testing;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Servlet filter for setting default user authentication
+ */
+@AllArgsConstructor
+public class DefaultUserFilter extends OncePerRequestFilter {
+
+    private final Authentication authentication;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+        securityContext.setAuthentication(authentication);
+        SecurityContextHolder.setContext(securityContext);
+
+//        var session = request.getSession(true);
+//        session.setAttribute(SPRING_SECURITY_CONTEXT_KEY, securityContext);
+
+        filterChain.doFilter(request, response);
+
+    }
+
+}
