5b3079a4eb3a86706ab2556321400499d0789f70
Andrii Kuzmin <andrii.kuzmin@tesco.com>
2022-02-11 13:37:02 +0200

    PMA-1291 CR - Executive role to be automatically assigned. New tests were implemented.

---
+++ codebase/security/src/test/java/com/tesco/pma/colleague/security/TestDataUtils.java
@@ -0,0 +1,48 @@
+package com.tesco.pma.colleague.security;
+
+import com.tesco.pma.colleague.profile.domain.ColleagueEntity;
+import com.tesco.pma.colleague.security.domain.Account;
+import com.tesco.pma.colleague.security.domain.AccountStatus;
+import com.tesco.pma.colleague.security.domain.Role;
+import lombok.experimental.UtilityClass;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.UUID;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+@UtilityClass
+public class TestDataUtils {
+
+    public static Account buildAccount() {
+        var account = new Account();
+        account.setStatus(AccountStatus.ENABLED);
+        return account;
+    }
+
+    public static ColleagueEntity buildColleagueEntity(UUID uuid) {
+        var colleagueEntity = new ColleagueEntity();
+        colleagueEntity.setUuid(uuid);
+        return colleagueEntity;
+    }
+
+    public static ColleagueEntity.WorkLevel buildWorkLevel(String workLevelCode) {
+        var workLevel = new ColleagueEntity.WorkLevel();
+        workLevel.setCode(workLevelCode);
+        return workLevel;
+    }
+
+    public static Collection<Role> buildRoles() {
+        return Stream.of("PeopleTeam", "TalentAdmin")
+                .map(code -> {
+                    var role = new Role();
+                    role.setCode(code);
+                    return role;
+                })
+                .sorted(Comparator.comparing(Role::getCode))
+                .collect(toList());
+    }
+
+}
--- codebase/security/src/test/java/com/tesco/pma/colleague/security/rolefetch/RoleFetchServiceImplTest.java
+++ codebase/security/src/test/java/com/tesco/pma/colleague/security/rolefetch/RoleFetchServiceImplTest.java
@@ -1,15 +1,14 @@
 package com.tesco.pma.colleague.security.rolefetch;
 
-import com.tesco.pma.colleague.profile.domain.ColleagueEntity;
 import com.tesco.pma.colleague.profile.service.ProfileService;
 import com.tesco.pma.colleague.security.LocalTestConfig;
 import com.tesco.pma.colleague.security.domain.Account;
 import com.tesco.pma.colleague.security.domain.AccountStatus;
 import com.tesco.pma.colleague.security.service.UserManagementService;
-import com.tesco.pma.security.UserRoleNames;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
@@ -19,9 +18,15 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Stream;
 
+import static com.tesco.pma.colleague.security.TestDataUtils.buildAccount;
+import static com.tesco.pma.colleague.security.TestDataUtils.buildColleagueEntity;
+import static com.tesco.pma.colleague.security.TestDataUtils.buildRoles;
+import static com.tesco.pma.colleague.security.TestDataUtils.buildWorkLevel;
+import static com.tesco.pma.security.UserRoleNames.*;
 import static java.util.stream.Collectors.toList;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertIterableEquals;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ActiveProfiles("test")
@@ -43,13 +48,26 @@ class RoleFetchServiceImplTest {
 
     private static final UUID COLLEAGUE_UUID = UUID.randomUUID();
 
-    private static final Account ACCOUNT = account();
+    private static final Account ACCOUNT = buildAccount();
 
     private static final String ROLE_PREFIX = "ROLE_";
 
     @Test
+    void findRolesForColleagueShouldReturnEmptySetRoles() {
+        var account = buildAccount();
+        account.setStatus(AccountStatus.DISABLED);
+
+        when(mockUserManagementService.findAccountByColleagueUuid(COLLEAGUE_UUID))
+                .thenReturn(account);
+
+        var roles = roleFetchService.findRolesInAccountStorage(COLLEAGUE_UUID);
+
+        assertEquals(0, roles.size());
+    }
+
+    @Test
     void findRolesForColleagueShouldReturnOneRole() {
-        var colleagueEntity = colleagueEntity(COLLEAGUE_UUID);
+        var colleagueEntity = buildColleagueEntity(COLLEAGUE_UUID);
 
         when(mockUserManagementService.findAccountByColleagueUuid(COLLEAGUE_UUID))
                 .thenReturn(ACCOUNT);
@@ -60,14 +78,14 @@ class RoleFetchServiceImplTest {
 
         assertEquals(1, roles.size());
 
-        var expectedRoles = expectedRoles(UserRoleNames.COLLEAGUE);
+        var expectedRoles = expectedRoles(COLLEAGUE);
         assertIterableEquals(expectedRoles, roles);
 
     }
 
     @Test
     void findRolesForColleagueWithIsManagerTrueShouldReturnTwoRoles() {
-        var colleagueEntity = colleagueEntity(COLLEAGUE_UUID);
+        var colleagueEntity = buildColleagueEntity(COLLEAGUE_UUID);
         colleagueEntity.setManager(true);
 
         when(mockUserManagementService.findAccountByColleagueUuid(COLLEAGUE_UUID))
@@ -79,14 +97,15 @@ class RoleFetchServiceImplTest {
 
         assertEquals(2, roles.size());
 
-        var expectedRoles = expectedRoles(UserRoleNames.COLLEAGUE, UserRoleNames.LINE_MANAGER);
+        var expectedRoles = expectedRoles(COLLEAGUE, LINE_MANAGER);
         assertIterableEquals(expectedRoles, roles);
 
     }
 
     @Test
     void findRolesForColleagueWithWorkLevelWL3ShouldReturnTwoRoles() {
-        var colleagueEntity = colleagueEntityWithWorkLevel("WL3");
+        var colleagueEntity = buildColleagueEntity(COLLEAGUE_UUID);
+        colleagueEntity.setWorkLevel(buildWorkLevel("WL3"));
 
         when(mockUserManagementService.findAccountByColleagueUuid(COLLEAGUE_UUID))
                 .thenReturn(ACCOUNT);
@@ -97,14 +116,15 @@ class RoleFetchServiceImplTest {
 
         assertEquals(2, roles.size());
 
-        var expectedRoles = expectedRoles(UserRoleNames.COLLEAGUE, UserRoleNames.EXECUTIVE);
+        var expectedRoles = expectedRoles(COLLEAGUE, EXECUTIVE);
         assertIterableEquals(expectedRoles, roles);
 
     }
 
     @Test
     void findRolesForColleagueWithWorkLevelWL1ShouldReturnOneRole() {
-        var colleagueEntity = colleagueEntityWithWorkLevel("WL1");
+        var colleagueEntity = buildColleagueEntity(COLLEAGUE_UUID);
+        colleagueEntity.setWorkLevel(buildWorkLevel("WL1"));
 
         when(mockUserManagementService.findAccountByColleagueUuid(COLLEAGUE_UUID))
                 .thenReturn(ACCOUNT);
@@ -115,30 +135,39 @@ class RoleFetchServiceImplTest {
 
         assertEquals(1, roles.size());
 
-        var expectedRoles = expectedRoles(UserRoleNames.COLLEAGUE);
+        var expectedRoles = expectedRoles(COLLEAGUE);
         assertIterableEquals(expectedRoles, roles);
 
     }
 
-    private static Account account() {
-        var account = new Account();
-        account.setStatus(AccountStatus.ENABLED);
-        return account;
+    @Test
+    void findRolesForColleagueWithTwoAccountRolesShouldReturnThreeRoles() {
+        var account = buildAccount();
+        account.setRoles(buildRoles());
+
+        var colleagueEntity = buildColleagueEntity(COLLEAGUE_UUID);
+
+        when(mockUserManagementService.findAccountByColleagueUuid(COLLEAGUE_UUID))
+                .thenReturn(account);
+        when(mockRolesMapper.findRoleByCode(anyString()))
+                .thenAnswer(
+                        (Answer<String>) invocation -> {
+                            Object[] args = invocation.getArguments();
+                            return ROLE_PREFIX + args[0];
+                        });
+        when(mockProfileService.getColleague(COLLEAGUE_UUID))
+                .thenReturn(colleagueEntity);
+
+        var roles = roleFetchService.findRolesInAccountStorage(COLLEAGUE_UUID);
+        var sortedRoles = roles.stream().sorted().collect(toList());
+
+        assertEquals(3, sortedRoles.size());
+
+        var expectedRoles = expectedRoles(COLLEAGUE, PEOPLE_TEAM, TALENT_ADMIN);
+        assertIterableEquals(expectedRoles, sortedRoles);
+
     }
 
-    private ColleagueEntity colleagueEntity(UUID uuid) {
-        var colleagueEntity = new ColleagueEntity();
-        colleagueEntity.setUuid(uuid);
-        return colleagueEntity;
-    }
-
-    private ColleagueEntity colleagueEntityWithWorkLevel(String workLevelCode) {
-        var colleagueEntity = new ColleagueEntity();
-        var workLevel = new ColleagueEntity.WorkLevel();
-        workLevel.setCode(workLevelCode);
-        colleagueEntity.setWorkLevel(workLevel);
-        return colleagueEntity;
-    }
 
     private List<String> expectedRoles(String... names) {
         return Stream.of(names).map(name -> ROLE_PREFIX + name).collect(toList());
