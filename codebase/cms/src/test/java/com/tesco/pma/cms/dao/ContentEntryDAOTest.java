87fbef02c4064e7b2600bbd67667b6d186548231
lxdi <alexfordeal@gmail.com>
2022-02-15 17:07:14 +0300

    PMA-1313 CMS

--- codebase/cms/src/test/java/com/tesco/pma/cms/dao/ContentEntryDAOTest.java
+++ codebase/cms/src/test/java/com/tesco/pma/cms/dao/ContentEntryDAOTest.java
@@ -11,14 +11,17 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 public class ContentEntryDAOTest extends AbstractDAOTest {
 
     private static final String BASE_PATH_TO_DATA_SET = "com/tesco/pma/cms/dao/";
     private static final String TEST_CONTENT_UUID = "d9d819fc-c1ee-4df8-a87b-d88f1c006c11";
+    private static final String TEST_CONTENT_UUID_2 = "a49bee82-71bb-4cad-b698-24422fb2fb29";
 
     @Autowired
     private ContentEntryDAO contentEntryDAO;
@@ -36,6 +39,7 @@ public class ContentEntryDAOTest extends AbstractDAOTest {
         var content = new ContentEntry();
         content.setId(UUID.randomUUID());
         content.setStatus(ContentStatus.DRAFT);
+        content.setTitle("test3");
         content.setKey("some/key");
         content.setImageLink("image/link");
         content.setCreatedBy(UUID.randomUUID());
@@ -49,13 +53,13 @@ public class ContentEntryDAOTest extends AbstractDAOTest {
     void deleteTest(){
         contentEntryDAO.delete(UUID.fromString(TEST_CONTENT_UUID));
 
-        assertEquals(0, contentEntryDAO.findByKey("knowledge-library/gb/content").size());
+        assertNull(contentEntryDAO.findById(UUID.fromString(TEST_CONTENT_UUID)));
     }
 
     @Test
     @DataSet({BASE_PATH_TO_DATA_SET + "contents.xml"})
     void findByKeyTest() {
-        assertEquals(1, contentEntryDAO.findByKey("knowledge-library/gb/content").size());
+        assertEquals(3, contentEntryDAO.findByKey("knowledge-library/gb/content").size());
     }
 
     @Test
@@ -66,7 +70,9 @@ public class ContentEntryDAOTest extends AbstractDAOTest {
 
         contentEntryDAO.update(content);
 
-        assertEquals(ContentStatus.UNPUBLISHED, contentEntryDAO.findById(UUID.fromString(TEST_CONTENT_UUID)).getStatus());
+        content = contentEntryDAO.findById(UUID.fromString(TEST_CONTENT_UUID));
+        assertEquals(ContentStatus.UNPUBLISHED, content.getStatus());
+        assertEquals(3, content.getVersion());
     }
 
     @Test
@@ -76,7 +82,34 @@ public class ContentEntryDAOTest extends AbstractDAOTest {
         rq.addFilters("status_eq", "pUbLished");
         rq.addFilters("key_eq", "knowledge-library/gb/content");
 
+        assertEquals(3, contentEntryDAO.find(rq).size());
+
+    }
+
+    @Test
+    @DataSet({BASE_PATH_TO_DATA_SET + "contents.xml"})
+    void findLatestVersionTest() {
+        var rq = new RequestQuery();
+        rq.addFilters("title_eq", "test");
+        rq.addFilters("key_eq", "knowledge-library/gb/content");
+        rq.addFilters("version_eq", -1);
+
+        List<ContentEntry> contentList = contentEntryDAO.find(rq);
+        assertEquals(1, contentList.size());
+        assertEquals(TEST_CONTENT_UUID_2, contentEntryDAO.find(rq).get(0).getId().toString());
+
+    }
+
+    @Test
+    @DataSet({BASE_PATH_TO_DATA_SET + "contents.xml"})
+    void findByVersionTest() {
+        var rq = new RequestQuery();
+        rq.addFilters("title_eq", "test");
+        rq.addFilters("key_eq", "knowledge-library/gb/content");
+        rq.addFilters("version_eq", 1);
+
         assertEquals(1, contentEntryDAO.find(rq).size());
+        assertEquals(TEST_CONTENT_UUID, contentEntryDAO.find(rq).get(0).getId().toString());
 
     }
 
