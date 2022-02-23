9750f9af6649b03bf9843b1891ce2dcab5a2ce6d
mdiadia <mykyta.diadia@tesco.com>
2022-02-23 13:43:51 +0200

    PMA-1377

--- codebase/flow/src/main/java/com/tesco/pma/flow/handlers/FindCycleHandler.java
+++ codebase/flow/src/main/java/com/tesco/pma/flow/handlers/FindCycleHandler.java
@@ -2,6 +2,7 @@ package com.tesco.pma.flow.handlers;
 
 import com.tesco.pma.bpm.api.flow.ExecutionContext;
 import com.tesco.pma.bpm.camunda.flow.handlers.CamundaAbstractFlowHandler;
+import com.tesco.pma.configuration.NamedMessageSourceAccessor;
 import com.tesco.pma.cycle.api.PMCycleStatus;
 import com.tesco.pma.cycle.service.PMCycleService;
 import com.tesco.pma.flow.FlowParameters;
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.tesco.pma.cycle.service.PMCycleServiceImpl.ENTRY_CONFIG_KEY_CONDITION;
@@ -25,27 +27,40 @@ public class FindCycleHandler extends CamundaAbstractFlowHandler {
 
     private static final String STATUS_IN_CONDITION = STATUS_CONDITION + "_in";
     private static final String ALLOWED_STATUSES = "ALLOWED_STATUSES";
+    private static final String COUNT_PARAM = "count";
+    private static final String KEY_PARAM = "key";
+    private static final String STATUSES_PARAM = "statuses";
 
     private final PMCycleService pmCycleService;
+    private final NamedMessageSourceAccessor messageSourceAccessor;
 
     @Override
     protected void execute(ExecutionContext context) throws Exception {
         var configKey = context.getVariable(FlowParameters.PM_CYCLE_KEY, String.class);
-        var requestQuery = new RequestQuery();
-        requestQuery.addFilters(STATUS_IN_CONDITION, getStatuses(context));
-        requestQuery.addFilters(ENTRY_CONFIG_KEY_CONDITION, configKey);
+        var statuses = getStatuses(context);
+        var requestQuery = buildRequestQuery(configKey, statuses);
 
         var pmCycles = pmCycleService.getAll(requestQuery, false);
         if (pmCycles.size() == 1) {
             context.setVariable(FlowParameters.PM_CYCLE, pmCycles.get(0));
         } else {
-            throw new BpmnError(PM_CYCLE_MORE_THAN_ONE_IN_STATUSES.getCode());
+            throw new BpmnError(PM_CYCLE_MORE_THAN_ONE_IN_STATUSES.getCode(),
+                    messageSourceAccessor.getMessage(PM_CYCLE_MORE_THAN_ONE_IN_STATUSES, Map.of(
+                            COUNT_PARAM, pmCycles.size(),
+                            KEY_PARAM, configKey,
+                            STATUSES_PARAM, statuses)));
         }
+    }
+
+    private RequestQuery buildRequestQuery(String configKey, Collection<PMCycleStatus> statuses) {
+        var requestQuery = new RequestQuery();
+        requestQuery.addFilters(STATUS_IN_CONDITION, statuses);
+        requestQuery.addFilters(ENTRY_CONFIG_KEY_CONDITION, configKey);
+        return requestQuery;
     }
 
     private Collection<PMCycleStatus> getStatuses(ExecutionContext context) {
         List<String> params = context.getVariable(ALLOWED_STATUSES);
-
         return params.stream().map(p -> EnumUtils.getEnumIgnoreCase(PMCycleStatus.class, p)).collect(Collectors.toSet());
     }
 }
