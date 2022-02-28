84c5013b28283b4501fe03519cc4943a1c74f877
Victoria Silenko <Victoria.Silenko@tesco.com>
2022-02-28 17:06:09 +0200

    PMA-1470 pm cycle assignment - fix in tests

--- codebase/pm/src/test/java/com/tesco/pma/cycle/LocalTestConfig.java
+++ codebase/pm/src/test/java/com/tesco/pma/cycle/LocalTestConfig.java
@@ -2,12 +2,14 @@ package com.tesco.pma.cycle;
 
 import com.tesco.pma.configuration.MessageSourceConfig;
 import com.tesco.pma.configuration.NamedMessageSourceAccessor;
-import com.tesco.pma.error.ApiExceptionHandler;
 import com.tesco.pma.util.ResourceProvider;
 import com.tesco.pma.process.service.ClasspathResourceProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.context.MessageSourceAutoConfiguration;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 
 @Profile("test")
 @Configuration
