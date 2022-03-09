6556e6325bdd0de16ed2ecbf088a8c5b30a652e4
Igor Tsentylo <tsentylo@gmail.com>
2022-03-09 14:25:04 +0200

    feat/PMA-1405: Impl. app. props (p.11)

--- deployment/deploy-dev.sh
+++ deployment/deploy-dev.sh
@@ -2,9 +2,9 @@
 
 kubectl config use-context euw-dev-214-pma-aks-admin
 helm secrets upgrade \
+  --debug \
   --install pma-api-service ./pma-api/ \
   --set image.tag=$1 \
   -f ./pma-api/values-dev.yaml \
   -f ./pma-api/secrets-dev.yaml \
-  -f ./pma-api/dev/* \
   -n api
