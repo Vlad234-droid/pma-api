e2bf5f36b2d225cbfb19721c669bc224b3a1fcce
lxdi <alexfordeal@gmail.com>
2022-03-22 14:13:29 +0300

    PMA-1579 /pm-cycles/mapping/keys using DmnEngine

--- codebase/pm/src/main/java/com/tesco/pma/cycle/service/PmCycleMappingServiceImpl.java
+++ codebase/pm/src/main/java/com/tesco/pma/cycle/service/PmCycleMappingServiceImpl.java
@@ -2,21 +2,29 @@ package com.tesco.pma.cycle.service;
 
 import com.tesco.pma.bpm.util.DmnTablesUtils;
 import lombok.RequiredArgsConstructor;
-import org.camunda.bpm.engine.ProcessEngine;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collections;
 import java.util.Set;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class PmCycleMappingServiceImpl implements PmCycleMappingService {
 
+    private static final String PM_CYCLE_MAPPING_TABLE_PATH = "com/tesco/pma/flow/pm_cycle_mapping.dmn";
     private static final String PM_CYCLE_MAPPING_TABLE_KEY = "pm_cycle_mapping";
-
-    private final ProcessEngine processEngine;
 
     @Override
     public Set<String> getPmCycleMappingKeys() {
-        return DmnTablesUtils.getAllResults(processEngine.getRepositoryService(), PM_CYCLE_MAPPING_TABLE_KEY);
+        try (InputStream is = this.getClass().getClassLoader().getResourceAsStream(PM_CYCLE_MAPPING_TABLE_PATH)){
+            return DmnTablesUtils.getAllResults(is, PM_CYCLE_MAPPING_TABLE_KEY);
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+            return Collections.emptySet();
+        }
     }
 }
