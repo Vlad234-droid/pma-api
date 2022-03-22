20dcd277bb8995727213261869eeab9c9d5455f9
lxdi <alexfordeal@gmail.com>
2022-03-22 18:37:19 +0300

    PMA-1579

--- codebase/camunda/src/main/java/com/tesco/pma/bpm/util/DmnTablesUtils.java
+++ codebase/camunda/src/main/java/com/tesco/pma/bpm/util/DmnTablesUtils.java
@@ -3,15 +3,11 @@ package com.tesco.pma.bpm.util;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
-import org.camunda.bpm.dmn.engine.DmnDecision;
-import org.camunda.bpm.dmn.engine.DmnEngineConfiguration;
 import org.camunda.bpm.dmn.engine.impl.DmnDecisionResultException;
-import org.camunda.bpm.dmn.engine.impl.DmnDecisionTableImpl;
 import org.camunda.bpm.engine.RepositoryService;
 import org.camunda.bpm.model.dmn.instance.DecisionTable;
 import org.camunda.bpm.model.dmn.instance.Rule;
 
-import java.io.InputStream;
 import java.util.Collections;
 import java.util.Set;
 import java.util.function.Function;
@@ -27,25 +23,6 @@ public class DmnTablesUtils {
 
     public Set<String> getAllResults(RepositoryService repositoryService, String tableName) {
         return getAll(repositoryService, tableName, DmnTablesUtils::getResultText);
-    }
-
-    public Set<String> getAllResults(InputStream inputStream, String tableName) {
-
-        var dmnEngine = DmnEngineConfiguration
-                .createDefaultDmnEngineConfiguration()
-                .buildEngine();
-
-        DmnDecision decision = dmnEngine.parseDecision(tableName, inputStream);
-
-        if (!(decision.getDecisionLogic() instanceof DmnDecisionTableImpl)) {
-            log.warn("Decision type is {}", decision.getClass().getName());
-            return Collections.emptySet();
-        }
-
-        return ((DmnDecisionTableImpl)decision.getDecisionLogic()).getRules().stream()
-                .map(rule -> rule.getConclusions().get(0))
-                .map(c -> StringUtils.remove(c.getExpression(), '"'))
-                .collect(Collectors.toSet());
     }
 
     private Set<String> getAll(RepositoryService repositoryService, String tableName, Function<Rule, String> func) {
