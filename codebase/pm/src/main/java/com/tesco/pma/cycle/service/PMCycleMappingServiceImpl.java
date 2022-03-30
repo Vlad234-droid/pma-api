000d7520c854d5a22b7856a5924cfb507990bef3
lxdi <alexfordeal@gmail.com>
2022-03-30 16:02:05 +0300

    PMA-1603

--- codebase/pm/src/main/java/com/tesco/pma/cycle/service/PMCycleMappingServiceImpl.java
+++ codebase/pm/src/main/java/com/tesco/pma/cycle/service/PMCycleMappingServiceImpl.java
@@ -1,10 +1,12 @@
 package com.tesco.pma.cycle.service;
 
 import com.tesco.pma.bpm.util.DmnTablesUtils;
+import com.tesco.pma.colleague.api.Colleague;
 import com.tesco.pma.colleague.profile.service.ProfileService;
 import com.tesco.pma.flow.FlowParameters;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.variable.Variables;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Service
@@ -33,21 +36,14 @@ public class PMCycleMappingServiceImpl implements PMCycleMappingService {
 
     @Override
     public Map<UUID, String> getPmCycleMappingKeys(List<UUID> colleaguesUuids) {
-        return colleaguesUuids.stream().collect(Collectors.toMap(uuid -> uuid, this::getPmCycleMappingKey));
+        return colleaguesUuids.stream()
+                .map(profileService::findColleagueByColleagueUuid)
+                .filter(Objects::nonNull)
+                .filter(c -> !CollectionUtils.isEmpty(c.getWorkRelationships()))
+                .collect(Collectors.toMap(Colleague::getColleagueUUID, this::getPmCycleMappingKey));
     }
 
-    private String getPmCycleMappingKey(UUID colleagueUuid) {
-        var colleague = profileService.findColleagueByColleagueUuid(colleagueUuid);
-
-        if (colleague == null) {
-            log.info("Colleague {} not found", colleagueUuid);
-            return "";
-        }
-
-        if (CollectionUtils.isEmpty(colleague.getWorkRelationships())) {
-            log.info("Colleague {} has no workRelationship", colleagueUuid);
-            return "";
-        }
+    private String getPmCycleMappingKey(Colleague colleague) {
 
         var decisionService = processEngine.getDecisionService();
         var variables = Variables.createVariables().putValue(FlowParameters.COLLEAGUE.name(), colleague);
@@ -55,7 +51,6 @@ public class PMCycleMappingServiceImpl implements PMCycleMappingService {
         var result =
                 decisionService.evaluateDecisionTableByKey(PM_CYCLE_MAPPING_TABLE_KEY, variables);
 
-        var res = (String) result.getSingleEntry();
-        return res == null ? "" : res;
+        return StringUtils.defaultString(result.getSingleEntry());
     }
 }
--- codebase/pm/src/main/java/com/tesco/pma/cycle/service/rest/PMCycleEndpoint.java
+++ codebase/pm/src/main/java/com/tesco/pma/cycle/service/rest/PMCycleEndpoint.java
@@ -350,7 +350,7 @@ public class PMCycleEndpoint {
     @ApiResponse(responseCode = HttpStatusCodes.OK, description = "Found performance cycle mapping keys per colleagues")
     @ApiResponse(responseCode = HttpStatusCodes.NOT_FOUND, description = "Performance cycle mapping keys not found",
             content = @Content)
-    @GetMapping(value = "/pm-cycles/mappings/keys/per/colleagues", produces = APPLICATION_JSON_VALUE)
+    @GetMapping(value = "/pm-cycles/mappings/keys/colleagues", produces = APPLICATION_JSON_VALUE)
     public RestResponse<Map<UUID, String>> getPmCycleMappingKeyByColleagues(@RequestParam List<UUID> uuids) {
         return success(pmCycleMappingService.getPmCycleMappingKeys(uuids));
     }
